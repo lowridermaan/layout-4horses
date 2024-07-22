339b4b106872fe622601c188fd7dc50cd10e615d
Alex <lowridermaan@gmail.com>
2024-07-22 20:31:52 +0300

    сделал хедер и наисал скрипт для бесконечной строки

---
+++ js/script.js
@@ -0,0 +1,18 @@
+function Marquee(selector, speed) {
+  const parentSelector = document.querySelector(selector);
+  const clone = parentSelector.innerHTML;
+  const firstElement = parentSelector.children[0];
+  let i = 0;
+  console.log(firstElement);
+  parentSelector.insertAdjacentHTML('beforeend', clone);
+
+  setInterval(function () {
+    firstElement.style.marginLeft = `-${i}px`;
+    if (i > firstElement.clientWidth) {
+      i = 0;
+    }
+    i = i + speed;
+  }, 0);
+}
+
+window.addEventListener('load', Marquee('.scroll-line', 0.2));
